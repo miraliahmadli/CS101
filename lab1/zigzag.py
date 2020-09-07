ba4f457b6a6672c740b90382f1d0d39cb007d5cb
Mirali Ahmadli <miraliahmadli@gmail.com>
2020-09-07 21:28:36 +0900

    Lab1

---
+++ lab1/zigzag.py
@@ -0,0 +1,46 @@
+import sys
+sys.path.append("/home/mirali/dev/cs101-spr2020")
+
+from cs101_libraries_py35.cs1robots import *
+create_world()
+
+hubo = Robot()
+hubo.set_trace("blue")
+hubo.turn_left()
+
+def turn_right():
+    for _ in range(3):
+        hubo.turn_left()
+
+
+def right_turn():
+    turn_right()
+    try:
+        hubo.move()
+    except:
+        return False
+    turn_right()
+    return True
+
+
+def left_turn():
+    hubo.turn_left()
+    try:
+        hubo.move()
+    except:
+        return False
+    hubo.turn_left()
+    return True
+
+
+def zigzag(is_right_turn=True):
+    while hubo.front_is_clear():
+        hubo.move()
+    if is_right_turn:
+        succ = right_turn()
+    else:
+        succ = left_turn()
+    if succ:
+        zigzag(not is_right_turn)
+
+zigzag()
